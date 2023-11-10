3fa6fc9b2d843b04aa992c8378e507e63646825c
nathan hensel <nathanihensel@gmail.com>
2023-11-10 16:22:21 -0600

    tofu fmt

--- databases.tf
+++ databases.tf
@@ -1,6 +1,6 @@
 # tiers can be found at https://docs.digitalocean.com/reference/api/api-reference/#tag/Databases
 module "dev" {
-  name     = "dev"
-  source   = "./modules/do-postgres"
-  tier     = "db-s-1vcpu-1gb"
+  name   = "dev"
+  source = "./modules/do-postgres"
+  tier   = "db-s-1vcpu-1gb"
 }
